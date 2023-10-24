66cb18016e3d7e38730fc6fa3729261dc9e0ceed
黄万涛 <2747964040@qq.com>
2023-10-24 18:52:27 +0800

    feat: 设置弹窗，支持排序

--- miniprogram/components/Sort/Sort.ts
+++ miniprogram/components/Sort/Sort.ts
@@ -1,1 +1,18 @@
-Component({})
+Component({
+  data: {
+    visible: false
+  },
+
+
+  methods: {
+    handlePopup() {
+      this.setData({ visible: true });
+    },
+    onVisibleChange(e: any) {
+      this.setData({
+        visible: e.detail.visible,
+      });
+    },
+
+  },
+});
